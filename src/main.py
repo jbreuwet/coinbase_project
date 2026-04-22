31331d8a274771c61cff5e8a6fd09f2ab0eb8f5b
Josh Breuwet <jbreuwet@gmail.com>
2026-04-22 10:21:37 -0400

    Phase 3 complete, created a consumer script to parse through RabbitMQ queue and load the raw json data into the Postgres Bronze layer

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import asyncio
 from logger_config import setup_logger
-from producer import run
+from producer import run as producer_run
+from consumer import run as consumer_run
 
 logger = setup_logger()
 
+async def main():
+    logger.info("Starting pipeline - Phase 3 test")
+    await asyncio.gather(
+        producer_run(),
+        consumer_run()
+    )
+    
 if __name__ == "__main__":
     logger.info("Starting pipeline - Phase 2 test")
-    asyncio.run(run())
+    asyncio.run(main())
+    
